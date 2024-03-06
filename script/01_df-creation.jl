cdc651912f713aa7ee5dd7d8bb26049208a072d7
Yoshinobu Ishizaki <yoshinobu.ishizaki@music.yamaha.com>
2024-03-07 08:12:22 +0900

    julia版データフレーム作成終了

---
+++ script/01_df-creation.jl
@@ -0,0 +1,79 @@
+using Glob
+using DataFrames
+using Dates
+using CSV
+
+upnote_path = "./UpNote/General Space"
+
+function convtxt(path)
+    s = ""
+    update_dt = ""
+    create_dt = ""
+    cat_mode = false
+    isheader = true
+    categories = String[]
+    bdytxt = String[]
+    tags = String[]
+
+    txt = readlines(path)
+
+    for i in 1:length(txt)
+        if i > 1
+            # skip first line
+            s = txt[i]
+            if startswith(s,"date:")
+                s2 = replace(s, "date: " => "")
+                update_dt = DateTime(s2, DateFormat("yyyy-mm-dd HH:MM:SS"))
+            elseif startswith(s,"created: ")
+                s2 = replace(s, "created: " => "")
+                create_dt = DateTime(s2, DateFormat("yyyy-mm-dd HH:MM:SS"))
+            elseif startswith(s,"categories:")
+                cat_mode = true
+                
+            elseif s == "---"
+                isheader = false
+                cat_mode = false
+                continue
+            else
+                if cat_mode
+                    s2 = replace(s, r"^- " => "")
+                    push!(categories, s2)
+                end
+            end
+            # start of body parser
+            if !isheader 
+                if startswith(s,r"#+ ")
+                    s2 = replace(s, r"#+ " => "")
+                    push!(bdytxt,s2)
+                elseif contains(s, r"^[\s\*]+$")
+                    s2 = replace(s, r"^[\s\*]+$" => "")
+                    push!(bdytxt,s2)
+                elseif contains(s,r"^#[^[:punct:] ]*$")
+                    s2 = replace(s,r"^#" => "")
+                    push!(tags, s2)
+                else
+                    push!(bdytxt,s)
+                end
+            end # body
+        end # i > 1
+    end # i
+    
+    cat_str = join(categories, "|")
+    contents = join(bdytxt, "\n")
+    tags_str = join(tags, "|")
+    
+    return(Dict("fpath" => basename(path), "update" => update_dt, "created" => create_dt, "category" => cat_str, "tags" => tags_str, "contents" => contents))
+end
+
+# do excecution
+
+tfiles = glob("*.md", upnote_path)
+
+dfm = DataFrame()
+ 
+for f in tfiles
+    append!(dfm, convtxt(f))
+end
+
+CSV.write("data/upnote_text.csv", dfm)
+
